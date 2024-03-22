66e17e69262ccff3f4f535c640b036ca9c69e968
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-03-22 21:43:02 +0500

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./xmrig --coin zephyr --url "pool.hashvault.pro:8888" --user ZEPHYR33CSf6UDWwcAQxD6JzXjbv39Z1j9bHLmDkfdBQH6bt9VjvjdPcYQXfGHkRVRERa5PJyqkiXaGdoMRh17zfj4GnfQzD9F41U -p x -k
